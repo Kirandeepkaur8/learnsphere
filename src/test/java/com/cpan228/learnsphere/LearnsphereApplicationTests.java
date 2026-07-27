d4b62e44a5abbe4498c412a89ff643f71d4594ac
Kirandeep Kaur <kiranbhullara@gmail.com>
2026-07-26 22:16:03 -0400

    Initialize LearnSphere Spring Boot project

---
+++ src/test/java/com/cpan228/learnsphere/LearnsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpan228.learnsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
